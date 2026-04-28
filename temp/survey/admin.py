db9ea6d134ccd6bf369c2ddb04a1e13e4b6619d5
Sham-Agha <w1993122@westminster.ac.uk>
2026-04-28 19:00:13 +0100

    deleted temp

--- temp/survey/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Question, Choice, Video, Plan, PlanOptions
-# Register your models here.
-admin.site.register(Question)
-admin.site.register(Choice)
-admin.site.register(Video)
-admin.site.register(Plan)
-admin.site.register(PlanOptions)
-
